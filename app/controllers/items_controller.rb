8c6e163f1aa4d0c9f95111f0c84317bed5afe456
saya0714 <38k.xxxx.3838@gmail.com>
2021-07-02 15:38:52 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_tweet, only: [:edit, :show]
+
 
   def index
     @items = Item.order(created_at: "DESC")
@@ -17,12 +19,36 @@ class ItemsController < ApplicationController
   end
 end
 
-def show
-  @item = Item.find(params[:id])
+  def show
+    @item = Item.find(params[:id])
+    end
+
+  def update
+    if item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path
+  else
+    render :edit
+  end
 end
+
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+     redirect_to root_path 
+    else
+      render :edit
+    end
+    end
+
+
 
 private
 def item_params
   params.require(:item).permit(:title, :text, :category_id, :status_id, :price, :delivery_area_id, :delivery_charge_id, :delivery_day_id, :image).merge(user_id: current_user.id)
   end
+end
+
+def set_tweet
+  @item = Item.find(params[:id])
 end
