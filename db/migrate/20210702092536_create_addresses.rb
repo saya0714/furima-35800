339017a39b9637575221a6df177e86943e4acca4
saya0714 <38k.xxxx.3838@gmail.com>
2021-07-06 17:30:52 +0900

    first commit

---
+++ db/migrate/20210702092536_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string     :postal_code,      null: false
+      t.integer    :delivery_area_id, null: false
+      t.string     :city_name,        null: false
+      t.string     :address,          null: false
+      t.string     :building_name,    null: false
+      t.string     :phone_number,     null: false
+      t.references :history,          null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
